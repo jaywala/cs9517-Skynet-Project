b65e628ee1e5a82d8647efcacf05fceb16c5b68e
Jay Motwani <jymotwani@hotmail.com>
2018-10-24 19:42:59 +1100

    ekf bugs fixed and tested

--- EKF.py
+++ EKF.py
@@ -1,41 +1,47 @@
 import numpy as np
+H = np.array([[1., 0., 0., 0.],
+                [0., 1., 0., 0]])
+
+R = np.array([[0.1**2., 0.], [0., 0.1**2.]])
 
 def predict(X0,P,dt):
     Xe = np.array([0,0,0,0])[np.newaxis,:].T
+    F = np.array([[1., 0, dt, 0.], 
+                [0., 1., 0., dt],
+                [0., 0., 1., 0.],
+                [0., 0., 0., 1.]])
+    Q = np.array([[0.4**2., 0., 0., 0.], 
+                [0., 0.4**2, 0., 0.], 
+                [0., 0., (3*dt)**2, 0], 
+                [0., 0., 0., (3*dt)**2]])
+
     # print(Xe)
     # print("Xe")
-    F = np.array([[1., 0, dt, 0.], 
-                    [0., 1., 0., dt],
-                    [0., 0., 1., 0.],
-                    [0., 0., 0., 1.]])
-    Q = np.array([[0.4**2., 0., 0., 0.], 
-                    [0., 0.4**2, 0., 0.], 
-                    [0., 0., (3*dt)**2, 0], 
-                    [0., 0., 0., (3*dt)**2]])
+
     Xe = np.dot(F,X0)
     P = np.linalg.multi_dot([F,P,np.transpose(F)]) + Q
     return (Xe,P)
     
 
 def ApplyEKF(X0,P,dt,Ymeasured,Xmeasured):
-    Xe = np.array([0,0,0,0])[np.newaxis,:].T
+    Xe = np.array([0.,0.,0.,0.])[np.newaxis,:].T
+    F = np.array([[1., 0, dt, 0.], 
+                [0., 1., 0., dt],
+                [0., 0., 1., 0.],
+                [0., 0., 0., 1.]])
+    Q = np.array([[0.4**2., 0., 0., 0.], 
+                [0., 0.4**2, 0., 0.], 
+                [0., 0., (3*dt)**2, 0], 
+                [0., 0., 0., (3*dt)**2]])
+
     # print(Xe)
     # print("Xe")
-    F = np.array([[1., 0, dt, 0.], 
-                    [0., 1., 0., dt],
-                    [0., 0., 1., 0.],
-                    [0., 0., 0., 1.]])
-    Q = np.array([[0.4**2., 0., 0., 0.], 
-                    [0., 0.4**2, 0., 0.], 
-                    [0., 0., (3*dt)**2, 0], 
-                    [0., 0., 0., (3*dt)**2]])
     Xe = np.dot(F,X0)
     P = np.linalg.multi_dot([F,P,np.transpose(F)]) + Q
+
     # Step 2: Measurement Prediction
     # H(1) = y = y + vy*dt
     # H(2) = z = z + vz*dt
-    H = np.array([[1., 0., 0., 0.],
-                    [0., 1., 0., 0]])
     Yexpected = Xe[0]
     Xexpected = Xe[1]
     # Ymeasured (pixel from sensor)
@@ -47,7 +53,6 @@ def ApplyEKF(X0,P,dt,Ymeasured,Xmeasured):
     z = np.array([ydiff, xdiff])[np.newaxis,:].T
     
     # Measurement Prediction Covariance: s
-    R = np.array([[0.1**2., 0.], [0., 0.1**2.]])
     s = np.linalg.multi_dot([H,P,np.transpose(H)]) + R
     # print(s)
     iS = np.linalg.inv(s)
@@ -63,7 +68,7 @@ def ApplyEKF(X0,P,dt,Ymeasured,Xmeasured):
     step = np.dot(Gain,z)
     # print("Step")
     # print(step)
-    for i in range(0,3):
+    for i in range(0,4):
         Xe[i] = Xe[i] + step[i]
     P = P - np.linalg.multi_dot([P,H.T,iS,H,P])
     # print("XE")
